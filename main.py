d3fa661af8949de994cad675a9cf3abf8dd9ec5f
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-31 12:01:09 -0500

    ejercicio masa corporal

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-#triangulos
-def tipo_triangulo(a, b, c):
-    # Verificar si es un triángulo válido
-    if a + b > c and a + c > b and b + c > a:
-        # Determinar el tipo de triángulo
-        if a == b == c:
-            return "El triángulo es equilátero."
-        elif a == b or a == c or b == c:
-            return "El triángulo es isósceles."
-        else:
-            return "El triángulo es escaleno."
-    else:
-        return "No es un triángulo válido."
+import math
+height=float(input("Enter your heigth"))
+weight=float(input("Enter your weigth"))
+age=float(input("Enter your age"))
 
-# Ciclo para permitir múltiples entradas
-while True:
-    try:
-        a = float(input("Ingrese a: "))
-        b = float(input("Ingrese b: "))
-        c = float(input("Ingrese c: "))
-        
-        # Verificar el tipo de triángulo
-        resultado = tipo_triangulo(a, b, c)
-        print(resultado)
-        
-    except ValueError:
-        print("Error: Ingrese un número válido.")
-    
-    # Preguntar si desea continuar
-    continuar = input("¿Desea verificar otro triángulo? (s/n): ")
-    if continuar.lower() != 's':
-        break
+Imc =weight/math.pow(height,2)
+if(age<45):
+  if(Imc>22):
+      print("your low_risk status")
+  else: 
+       print("your low_risk status")
+else:
+    if(Imc<22):
+        print("medium risk condition")
+    else:
+        print("high risk condition")
