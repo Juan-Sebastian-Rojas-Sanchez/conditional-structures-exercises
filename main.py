fab0d3706ac958a98384bf50a15b6b3521644757
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-31 10:15:49 -0500

    ejercicio ordenamiento

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-#Palabra más larga
-# Solicitar al usuario que ingrese dos palabras
-palabra1 = input("Palabra 1: ")
-palabra2 = input("Palabra 2: ")
+#Ordenamiento
+# Solicitar al usuario que ingrese dos números
+numero1 = int(input("Ingrese numero: "))
+numero2 = int(input("Ingrese numero: "))
 
-# Calcular la longitud de las palabras
-longitud1 = len(palabra1)
-longitud2 = len(palabra2)
-
-# Comparar las longitudes y determinar el resultado
-if longitud1 > longitud2:
-    diferencia = longitud1 - longitud2
-    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
-elif longitud2 > longitud1:
-    diferencia = longitud2 - longitud1
-    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
+# Ordenar y mostrar los números
+if numero1 < numero2:
+    print(numero1, numero2)
 else:
-    print("Las dos palabras tienen el mismo largo.")
+    print(numero2, numero1)
