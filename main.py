842db64e191ca2b121ba3b0abac8fff60f56e891
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-31 10:13:14 -0500

    ejercicio palabra mas larga

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#División
-# Solicitar al usuario que ingrese el dividendo y el divisor
-dividendo = int(input("Dividendo: "))
-divisor = int(input("Divisor: "))
+#Palabra más larga
+# Solicitar al usuario que ingrese dos palabras
+palabra1 = input("Palabra 1: ")
+palabra2 = input("Palabra 2: ")
 
-# Realizar la división
-cociente = dividendo // divisor
-resto = dividendo % divisor
+# Calcular la longitud de las palabras
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
 
-# Verificar si la división es exacta
-if resto == 0:
-    print("La división es exacta.")
+# Comparar las longitudes y determinar el resultado
+if longitud1 > longitud2:
+    diferencia = longitud1 - longitud2
+    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
+elif longitud2 > longitud1:
+    diferencia = longitud2 - longitud1
+    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
 else:
-    print("La división no es exacta.")
-
-# Mostrar el cociente y el resto
-print(f"Cociente: {cociente}")
-print(f"Resto: {resto}")
+    print("Las dos palabras tienen el mismo largo.")
