09eaf73db29a2ebf6c896cb6a5d19a1348524574
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-31 10:09:40 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese un número
+numero = int(input("Ingrese un número: "))
+
+# Comprobar si el número es par o impar
+if numero % 2 == 0:
+    print("Su número es par")
+else:
+    print("Su número es impar")
+    
